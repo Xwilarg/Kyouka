f8b84f78091704c5c0cadee254eed8b4f9958b31
Xwilarg <xwilarg@yahoo.fr>
2020-11-15 01:13:08 +0100

    Add empty bot

---
+++ Kyouka/Module/Communication.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using DiscordUtils;
+using System.Threading.Tasks;
+
+namespace Kyouka.Module
+{
+    public class Communication : ModuleBase
+    {
+        [Command("Info")]
+        public async Task Info()
+        {
+            await ReplyAsync(embed: Utils.GetBotInfo(Program.P.StartTime, "Kyouka", Program.P.Client.CurrentUser));
+        }
+    }
+}
